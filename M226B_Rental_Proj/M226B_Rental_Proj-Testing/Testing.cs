7d6d063f7d0dc82d53a387eddaa16ebd55bcd238
Ardian Demiri <ademiri@online.gibz.ch>
2021-01-25 17:51:36 +0100

    Alles im neuen Projekt kopiert

---
+++ M226B_Rental_Proj/M226B_Rental_Proj-Testing/Testing.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+
+namespace M226B_Rental_Proj_Testing
+{
+    public class Tests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
